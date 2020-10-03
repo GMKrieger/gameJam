b04204e7be6f9f8ab9a2c3f172dadd6f2f809b0a
Gustavo Krieger <gkrieger@marlin.com.br>
2020-10-03 09:03:24 -0300

    Main Menu protoype, About section prototype

--- Assets/Controller/MainMenuController.cs
+++ Assets/Controller/MainMenuController.cs
@@ -1,9 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MainMenuController : MonoBehaviour
 {
+
+    public void playGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void about()
+    {
+        SceneManager.LoadScene("About");
+    }
+
+    public void title()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
