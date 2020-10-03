5f83e93d46579e16d814776ae4bcdf0231eb5fc0
Gustavo Krieger <gkrieger@marlin.com.br>
2020-10-03 10:23:49 -0300

    Started working on the game logic

---
+++ Assets/Service/GachaCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using UnityEngine;
+
+
+public class GachaCharacter : MonoBehaviour
+{
+
+    private string characterName;
+
+    public void setCharacterName(string name){
+        this.characterName = name;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Service/GachaLogic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using UnityEngine;
+
+
+public class GachaLogic : MonoBehaviour
+{
+
+    public GachaCharacter GetCharacter()
+    {
+        GachaCharacter gachaCharacter = new GachaCharacter();
+        int roll = UnityEngine.Random.Range(1, 1000);
+
+        if (roll <= 500) { //1 estrela
+            gachaCharacter.setCharacterName("1 estrela");
+        }
+        else if (roll <= 750) { //2 estrelas
+
+        }
+        else if (roll <= 900) { //3 estrelas
+
+        }
+        else if (roll <= 999) { //4 estrelas
+
+        }
+        else { //BRASIL-CHAN FTW
+
+        }
+
+        return gachaCharacter;
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
